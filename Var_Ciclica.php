4534d20e594f1bf5f4787938448323ae28a19250
Luis C. <Luismarval5a@hotmail.com>
2023-11-29 18:01:24 -0400

    Nuevos cambios

--- Var_Ciclica.php
+++ Var_Ciclica.php
@@ -1,11 +1,11 @@
 <?php
-    function Var_CiCli($Time,$I,$Desire,){
+    function Var_CiCli($Time,$I,$Desire){
         $Venta=0;$XAnon=$Desire/$Time;
         foreach($I as $valor){
             $Venta+=$valor;
         }
         $Xg=$Venta/$Time;
-        echo '<br><strong><span>X&#772;g = </span></strong>'.$Xg.'<br/><br/>';
+        echo '<strong><span>X&#772;g = </span></strong>'.$Xg.'<br/><br/>';
         foreach($I as $iteracion => $valor){
             $I[$iteracion]=$valor/$Xg;
             echo '<strong><span>I'.($iteracion+1).'= </span></strong>'.$I[$iteracion].'<br/>';
@@ -15,11 +15,23 @@
             echo '<strong><span>Xi'.($i+1).'= </span></strong>'.$Xt[$i].'<br/>';
         }
     }
-    if($_POST){
-        $Time=$_POST["Time"];$Desire=$_POST["Desire"];$I=[];
-        for($i=0;$i<$Time;$i++){
-            $I[$i]=$_POST['Num'.$i];
-        }
-    Var_CiCli($Time,$I,$Desire);
-    }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if($_POST){
+            $Time=$_POST["Time"];$Desire=$_POST["Desire"];$I=[];
+            for($i=0;$i<$Time;$i++){
+                $I[$i]=$_POST['Num'.$i];
+            }
+            Var_CiCli($Time,$I,$Desire);
+        }
+    ?>
+</body>
+</html>
